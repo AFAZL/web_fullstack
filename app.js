a2df628821ce68bd44c0745fb0a1cdf1815bd4b1
AFZAL <110601700+AFAZL@users.noreply.github.com>
2024-06-21 00:17:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,145 @@
+// console.log("Hello boiessss");
+// console.log("a"<"b");
+// let color="red";
+// if(color=="red"){
+//     console.log(`Rucko Zaraaaaa ${color} Haii`);
+// }
+// if(color=="yellow"){
+//     console.log("Start Karoooo");
+// }
+// if(color=="green"){
+//     console.log("Nekal fatfat");
+// }
+// let marks=90;
+// if(marks>80){
+//     console.log("OP Broooooooo");
+// }
+// else if(50<marks){
+//     console.log("Sai Haiiiiii");
+// }
+// else if(marks<50){
+//     console.log("Padh lay chomo");
+// }
+// if(marks>=35){
+//     if(marks>=80){
+//         console.log("O");
+//     }
+//     else{
+//         console.log("Pass");
+//     }
+// }
+// else{
+//     console.log("Failll");
+// }
+// let str="apple";
+// if(str[0]==="a" && str.length>=3){
+//     console.log(`The Given String ${str} is a Good String`);
+// }
+// marks=100;
+// switch(marks){
+//     case 100:
+//         console.log("PASSSSSSS");
+//         break;
+//     case 50:
+//         console.log("Thik haiiiiiiii");
+//         break;
+//     default:
+//         console.log("Not a correct value");
+// }
+// let name="Afzal and tommy";
+// console.log(name.toUpperCase());
+// console.log(name.slice(0,9));
+// let res=name.replace("A","z");
+// let rep=name.repeat(10);
+// console.log(rep);
+
+// let arr=["afzal",31,"Tommy",7];
+// arr.unshift("Affu");
+// arr.shift();
+
+// console.log(arr);
+
+// let mk=["AFFU","modak","tommy"];
+// mk[0]="afzal";
+// console.log(mk.concat(arr).sort().reverse());
+// let demo=[["X", null ,"O"],[null,"X",null],["O",null,"X"]];
+// console.log(demo);
+// let a=[1,2,3,4,5];
+// console.log(a.slice(0,4));
+
+// let e=prompt("Enter a number for table");
+// e=parseInt(e);
+// for(let k=e;k<=e*10;k=k+e){
+//     console.log(k);
+// }
+// let guess="MS";
+// let player=prompt("Gusse the player");
+// while(player!=guess && player!="quit"){
+//     console.log("Wrong!!!!");
+//     player=prompt("wrong , Gusse the player");
+// }
+// let fruits=[["apple","mango"],["banana","litchi"],["orange","graphs"]];
+// for(i of fruits){
+//     for(j of i){
+//         console.log(j);
+//     }
+// }
+// let todo=[];
+
+// while(true){
+//     let req=prompt("Enter yor req ");
+//     if(req=="quit"){
+//         console.log("you are Quiting");
+//         break;
+//     }
+//     if(req=="List"){
+//         console.log("______________________");
+//         for(i of todo){
+//             console.log(i);
+//         }
+//         console.log("______________________");
+//     }
+//     else if(req=="Add"){
+//         let v=prompt("Enetr the val u want to add");
+//         todo.push(v);
+//         console.log("Added Successful")
+//     }
+//     else if(req=="del"){
+//         let d=prompt("Enter the index u want to delete");
+//         todo.splice(d,1);
+//         console.log(`Element ${todo[d]} id Removed `);
+//     }
+
+// }
+// let op=[1,2,3,89,4,5,6,8,9];
+// let ans=0;
+// for(i of op){
+//     if(op[i]>ans){
+//         ans=op[i];
+//     }
+// }
+// console.log(ans);
+
+// const obj={
+//     s1 : {
+//         name:"Afzal",
+//         Age:21,
+//         Location:"Mumbai",
+//         Skills:["java","JS","Python"]
+//     },
+//     s2 : {
+//         name:"Affu",
+//         Age:22,
+//         Location:"Mumbai",
+//         Skills:["java","JS","Python"]
+//     },
+//     s3 : {
+//         name:"Tommy",
+//         Age:23,
+//         Location:"Mumbai",
+//         Skills:["java","JS","Python"]
+//     },
+// };
+
+// let x=Math.floor(Math.random()*5)+1;
+// console.log(x);
